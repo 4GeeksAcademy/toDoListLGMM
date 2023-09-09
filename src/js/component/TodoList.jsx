9c5f8aa1fb628a238b887664e878895376dcf712
Luis Guillermo <abg.lgmm@gmail.com>
2023-09-09 10:11:33 +0000

    commitLGMM

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,40 +1,58 @@
 import React, { useState } from "react";
 
 function TodoList() {
+  // 1. nombre de la variable
+  // 2. nombre de ka funcion que cambia la variable, le asigna el valor que tiene dentro del parentesis
+  // 3. asigno el valor inicial con lo que este dentro del parentesis del useState
   const [inputValue, setInputValue] = useState("");
   const [list, setList] = useState([]);
 
+  function handleInput(event) {
+    event.preventDefault();
+    if (inputValue.trim() !== "") {
+      setList([...list, inputValue]);
+      setInputValue("");
+    }else {
+      alert("El campo no puede estar vacío");
+    }
+  }
+
+  function handleRemoveItem(tarea) {
+    setList(list.filter((item, i) => tarea !== item));
+  } 
+
   return (
     <div>
-      <h1>My ToDos</h1>
+      <h1>My ToDo's</h1>
       <ul>
         <li>
-          <input
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyPress={(e) => {
-              if (e.key === "enter") {
-                console.log("enter was press");
-                setList(list.concat(inputValue));
-                setInputValue("")
-              }
-            }}
-            placeholder="¿Que necesitas hacer?"
-          />
+          <form onSubmit={handleInput}>
+            <input
+              type="text"
+              value={inputValue}
+              placeholder="¿Qué necesitas hacer?"
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </form>
         </li>
         {list.map((item, index) => (
-          <li>
-            {item} <i class="fas fa-trash" onClick={() => setList(list.filter((t, currenIndex) => index != currenIndex))}></i>
+          <li className="d-flex justify-content-between box" key={index}>
+            {item}
+            <span>
+              <i
+                className="fas fa-trash"
+                onClick={() => handleRemoveItem(item)}
+              ></i>
+            </span>
           </li>
         ))}
+        <li>
+          {list.length == 0
+            ? "No hay tareas, añadir tareas"
+            : `Tienes ${list.length} tareas pendientes.`}
+        </li>
       </ul>
-      <div>23 task</div>
     </div>
-    // crear una Ul
-    // En esa esa ul se debe agregar de forma dinamica los li al ingresar un input del lado del cliente
-    // Esa ul debe tener un li fijo que vaya diciendo la cantidad de li dinamicos que existen en la ul, o sea, sin contar el li fijo.
-    // agregar un boton a cada li que borre la li en la cual se hizo click.
   );
 }
 
